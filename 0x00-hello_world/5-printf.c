a2614ddf6559d33a98f7d000e73f3ebbe62b5488
Stephen Kariuki <stephenkariuki838@gmail.com>
2023-09-07 03:48:34 -0700

    task 5 on printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -8,5 +8,4 @@ int main(void)
 	  * the printf prints the given statement.
 	  */
 	printf("with proper grammar, but the outcome is a piece of art,")
-	return (0) /* this thing returns 0 as int*/ 
-}
+	return (0) /* this thing returns 0 as int.*/ }
