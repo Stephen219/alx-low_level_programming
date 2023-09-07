995d66f00b6d8fba58c37a28834ccc0f6ba76ba3
Stephen Kariuki <stephenkariuki838@gmail.com>
2023-09-07 02:58:48 -0700

    added more commits

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,13 @@
-/*
- * importing the header file
- */
 #include <stdio.h>
 /*
- * this is the main entry point of this c program
+ * this is the main entry point of this c program.
+ * @return the 0
  */
 int main(void)
 {
+	/*
+	 * prints the stated statement.
+	 */
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return 0; /* returns zero of the main function.*/
 }
