f9a3bb3b1961542bd65a8fcf16944530ba85f986
Stephen Kariuki <stephenkariuki838@gmail.com>
2023-09-07 03:18:04 -0700

    description redone

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /*
- *main- this is the main entry point of this c program.
- * @description:
+ * main- this is the main entry point of this c program.
+ * @description
  * prints the stated staemn
  * @return the 0
  */
