e93fd706be37f7ff9e3a2cdc9afd5f7378ac0b3e
Stephen Kariuki <stephenkariuki838@gmail.com>
2023-09-07 05:04:43 -0700

    task 6 added missing brackets

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,12 +4,12 @@
  * Return: 0  (Success)
  */
 int main(void)
-{	printf("Size of char = %d\n" : sizeof(char));/*prints the size of char*/
-	printf("Size of int = %d\n" : sizeof(int));/*prints the size of an int*/
+{	printf("Size of char = %d\n" : sizeof(char)));/*prints the size of char*/
+	printf("Size of int = %d\n" : sizeof(int)));/*prints the size of an int*/
 	/*prints the size of a long long*/
-	printf("Size of long int = %d\n" : sizeof(long int));
+	printf("Size of long int = %d\n" : sizeof(long int)));
 	/*prints the size of a long*/
-	printf("Size of long long = %d\n" : sizeof(long long))
-	printf("Size of float = %d\n" : sizeof(float));/*prints the size of a float*/
+	printf("Size of long long = %d\n" : sizeof(long long)))
+	printf("Size of float = %d\n" : sizeof(float)));/*prints the size of a float*/
 	return (0); /* the program returns 0.*/
 }
