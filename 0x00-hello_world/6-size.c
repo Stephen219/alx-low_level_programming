a609837be7551823989a912488d1871683e90822
Stephen Kariuki <stephenkariuki838@gmail.com>
2023-09-07 04:59:01 -0700

    it prints all the data types and sizes

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - the main entry of the program
+ * Return: 0  (Success)
+ */
+int main(void)
+{	printf("Size of char = %d\n" : sizeof(char));/*prints the size of char*/
+	printf("Size of int = %d\n" : sizeof(int));/*prints the size of an int*/
+	/*prints the size of a long long*/
+	printf("Size of long int = %d\n" : sizeof(long int));
+	/*prints the size of a long*/
+	printf("Size of long long = %d\n" : sizeof(long long))
+	printf("Size of float = %d\n" : sizeof(float));/*prints the size of a float*/
+	return (0); /* the program returns 0.*/
+}
