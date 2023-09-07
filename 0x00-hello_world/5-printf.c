f90b9565dc841428a15f8890c5571ddc3c990b74
Stephen Kariuki <stephenkariuki838@gmail.com>
2023-09-07 03:44:57 -0700

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio>
+/**
+ * main - the main entry method
+ * Return: 0 (Success)
+ */
+int main(void)
+{	/**
+	  * the printf prints the given statement.
+	  */
+	printf("with proper grammar, but the outcome is a piece of art,")
+	return (0) /* this thing returns 0 as int*/ 
+}
