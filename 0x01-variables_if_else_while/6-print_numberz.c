ea03a9d04402f4a7b7471411051c1cbbc0836999
Stephen Kariuki <stephenkariuki838@gmail.com>
2023-09-08 15:04:18 -0700

    task on numbers that is stask 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    for (int i = 0; i <= 9; i++) 
+    {
+        putchar(i + '0'); 
+    }
+
+    putchar('\n'); 
+
+    return (0);
+}
+
