1ea558d3506d7b4c5e0690d510598228c2a9ca69
Stephen Kariuki <stephenkariuki838@gmail.com>
2023-09-08 14:37:51 -0700

    task on numbers that is stask 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - the entry point of the project.
+ * Return: 0 (Succes)
+ * the method uses a for loop to get all the required numbers
+ */
+int main(void)
+{
+	for (int i = 0; 1 < 10 ; i++)
+	{
+		printf("%d\n", i);
+	}
+	return (0)
+}
