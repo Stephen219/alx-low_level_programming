d9c376c60a323c6d39ffec97b84313951b99cb6f
Stephen Kariuki <stephenkariuki838@gmail.com>
2023-09-08 10:11:29 -0700

    positive and negative inside assessed with an if condation

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -15,14 +15,14 @@ int main(void)
 	/* the if conditions that does the logic*/
 	if (n > 0)
 	{
-		printf("is positive \n")
+		printf("is positive \n");
 	} else if (n == 0)
 	{
-		printf("is zero \n")
+		printf("is zero \n");
 
 	} else
 	{
-		printf("is negative \n")
+		printf("is negative \n");
 	}
 	return (0);
 }
