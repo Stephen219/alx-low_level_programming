797b568607f175a37dd530fdea3460bf0085d129
Stephen Kariuki <stephenkariuki838@gmail.com>
2023-09-08 13:49:31 -0700

    task on last digit

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - entry to the program
+ * Return: 0 (ucces)
+ */
+
+int main(void)
+{
+ 	char lowercase = 'a';
+	char uppercase = 'A';
+
+	while (lowercase <= 'z')
+	{
+		putchar(lowercase);
+		lowercase++;
+    	}
+
+	while (uppercase <= 'Z')
+	{
+		putchar(uppercase);
+		uppercase++;
+	}
+
+	putchar('\n');
+	return (0);
+}
+
