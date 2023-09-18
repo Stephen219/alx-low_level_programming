f536e9b7bf0f2caa354bb506c96db8fdb629cafe
Stephen Kariuki <stephenkariuki838@gmail.com>
2023-09-18 16:19:12 -0700

    task 7 trial

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -9,22 +9,23 @@ void puts_half(char *str)
 {
 	int length = strlen(str);
 	int i, j;
-	switch (length % 2)
+	if (lenght % 2 == 0)
 	{
-		case 0:
-			j= length  / 2;
-			for (i  = j; str[i] != '\0';  i ++)
-			{
-				putchar(str[i]);
-			}
-			putchar('\n');
-			break;
-		default:
-			j = (length - 1) / 2;
-			for (i  = j; str[i] != '\0';  i ++)
-			{
-				putchar(str[i]);
-			}
-			putchar('\n');
+		j= length  / 2;
+		for (i  = j; str[i] != '\0';  i ++)
+		{
+			putchar(str[i]);
+		}
+		putchar('\n');
+
+	}else
+	{
+		j = (length - 1) / 2;
+		for (i  = j; str[i] != '\0';  i ++)
+		{
+			putchar(str[i]);
+		}
+		putchar('\n');
 	}
+	
 }
