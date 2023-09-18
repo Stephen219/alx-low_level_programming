96162732f48bac576ad0b0ba110fd9ceb1282397
Stephen Kariuki <stephenkariuki838@gmail.com>
2023-09-18 13:42:15 -0700

    task 4 on reverse strings   fixed betty

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 #include <string.h>
-/***
+/**
  * print_rev - the function that helpsa to print the string in reverse
  * Return: void in case of success
  * @s: the string.
@@ -12,7 +12,7 @@ void print_rev(char *s)
 	int i, size;
 	size = strlen(s);
 
-	for (i = size-1; i >= 0; i--)
+	for (i = size - 1; i >= 0; i--)
 	{
 		putchar(s[i]);
 	}
