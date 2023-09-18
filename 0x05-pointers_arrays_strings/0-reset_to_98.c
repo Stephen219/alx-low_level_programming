60c65e238399d0cc8b93536af01eb6a512665f03
Stephen Kariuki <stephenkariuki838@gmail.com>
2023-09-18 11:41:52 -0700

    question one and initial commit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+
+#include "main.h"
+#include <stdio.h>
+/**
+ * void reset_to_98(int *n) - the function that helpds to reset the n
+ * Return: void
+ * @n - this is the param it takes in.
+ */
+
+void reset_to_98(int *n){
+	*n=98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,2 @@
+this is the reaad me for this project about pointers and arrays in c
+
