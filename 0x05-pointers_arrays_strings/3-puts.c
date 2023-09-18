d718eeec78908fc08c2092e8c98f802c4e96be75
Stephen Kariuki <stephenkariuki838@gmail.com>
2023-09-18 13:04:08 -0700

    task 4

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _puts - the function that does the specified task
+ * Return: void
+ * @str: the param it takes.
+ */
+void _puts(char *str)
+{
+	for (int i = 0; i != '\0'; i++)
+	{
+		putchar(i);
+	}
+}
