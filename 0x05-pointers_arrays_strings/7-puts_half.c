572a49b1be1dc217a96fbaefd49418bae12d72cc
Stephen Kariuki <stephenkariuki838@gmail.com>
2023-09-18 15:09:25 -0700

    task 7 trial

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+/**
+ * puts_half - prints the other half of the string.
+ * @str: param
+ */
+void puts_half(char *str)
+{
+	int length = strlen(str);
+	int i, j, k;
+	switch (length % 2)
+	{
+		case 0:
+			j= length  / 2;
+			for (i  = j; i != '\0';  i ++)
+			{
+				putchar(str[i]);
+			}
+			putchar('\n');
+			break;
+		default:
+			j = (length - 1) / 2;
+			for (i  = j; i != '\0';  i ++)
+			{
+				putchar(str[i]);
+			}
+			putchar('\n');
+	}
+}
