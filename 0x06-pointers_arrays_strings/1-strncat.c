b0e575b22ea92722f0f0be0d6bd3a193d0cd33df
Stephen Kariuki <stephenkariuki838@gmail.com>
2023-09-21 06:58:38 -0700

    task 1 on strncat  the diff with strcat is the former has n

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -2,17 +2,17 @@
 #include <stdio.h>
 #include <string.h>
 /**
- * _strncat - this is the function concatinates the string.   
- * the differnce between strcat and this is that strncat 
+ * _strncat - this is the function concatinates the string.
+ * the differnce between strcat and this is that strncat
  * specifies the number of characters to be copied
  * Return: the pointer of the string incase of a succes
  * @dest:  the destination string
- * @source: the source string.
+ * @src: the source string.
  * @n:  the number of characters to be copied.
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int *ptr;
+	char *ptr;
 
 	strncat(dest, src, n);
 	ptr = &dest;
