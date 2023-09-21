4ba206a45cc397332d39a53b4d30cac2bf6017fe
Stephen Kariuki <stephenkariuki838@gmail.com>
2023-09-21 07:35:02 -0700

    task 3 on string compare

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *  _strcmp - this is the function thst compares the string.
+ *  Return: int 0 incase it is true but -ve if false.
+ *  @s1: first string
+ *  @s2: string2
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int ans = strcmp(s1, s2);
+
+	return (ans);
+}
