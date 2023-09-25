a39ff083fa439854c5cae247350b33aa82ca109f
Stephen Kariuki <stephenkariuki838@gmail.com>
2023-09-25 13:23:21 -0700

    task 6

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * print_chessboard - function that returns the chase the board
+ * Return: void
+ * @a: the rows
+ * @size: columns
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			putchar(a[i][j]);
+		}
+		putchar('\n')
+	}
+
+}
