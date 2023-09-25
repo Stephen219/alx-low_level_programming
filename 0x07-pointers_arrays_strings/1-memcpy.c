70d1a6869ad4c4d4b561a40603bca1b28d6685e9
Stephen Kariuki <stephenkariuki838@gmail.com>
2023-09-25 11:41:48 -0700

    task 1

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,17 @@
+
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * _memcpy - the main function of program
+ * Return: dest pointer
+ * @dest: destination
+ * @src: source
+ * @n: the size of the memory*/
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	memcpy(dest, src, n);
+	return (dest);
+
+}
