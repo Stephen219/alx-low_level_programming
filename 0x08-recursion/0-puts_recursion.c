a126034bfb683ba650f834107a701725be76200b
Stephen Kariuki <stephenkariuki838@gmail.com>
2023-09-27 10:59:22 -0700

    the first commit and task 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -9,12 +9,12 @@ void _puts_recursion(char *s)
 	if (*s == '\0')
 	{
 		return;
-		putchar('\n');
+		puts('\n');
 	} else
 	{
-		putchar(*s);
+		puts(*s);
 		_puts_recursion(s + 1);
 	}
-	putchar('\0');
+	puts('\0');
 
 }
