b2eff95e85aa3bc9ad63bc1f8323d2f6a6103377
Stephen Kariuki <stephenkariuki838@gmail.com>
2023-09-27 13:03:23 -0700

    task 4

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * factorial - this is the function of factorial
+ * @n: the param
+ * Return:  the answer.
+ */
+
+int factorial(int n)
+{
+	if (n >0)
+	{
+		return (-1);
+	}else if (n == 0)
+	{
+		return (1);
+	}else
+	{
+		return (n * factorial(n - 1));
+	}
+
+}
