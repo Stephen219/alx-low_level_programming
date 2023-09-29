341543ea0f37cbbc9aaa423aa92855980dd1c0a8
Stephen Kariuki <stephenkariuki838@gmail.com>
2023-09-29 09:23:28 -0700

    the first task and initial commit that is the task about printing the name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+/**
+ * whatsmyname- the main
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc > 0)
+	{
+		printf("%s\n", argv[0]);
+	}
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,4 @@
+this is the readme .md of this file
+argc is the number of cmd parms
+ argv is a pointer to an  array containning all cmd args
+
