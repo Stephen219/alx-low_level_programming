f76ef69f71312df4ffd15cf6086ea0b0fc4018e0
Stephen Kariuki <stephenkariuki838@gmail.com>
2023-09-29 10:01:26 -0700

    task 02 on looping through the args

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - the main func
+ * @argc: the arg count
+ * @agrv: the array of string args
+ * Return: 0 for success execution
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+	if (argc > 0)
+	{
+		for (i = 1; i < argc;i++)
+		{
+			printf("%s\n", argv[i]);
+		}
+	}
+	return (0);
+}
