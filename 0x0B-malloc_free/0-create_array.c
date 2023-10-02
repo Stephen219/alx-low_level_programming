4f77db40b0b6c6a5bc1765f4efe1ceba46c40ac8
Stephen Kariuki <stephenkariuki838@gmail.com>
2023-10-02 10:50:44 -0700

    task 1 and initial commit

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,30 @@
+
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * create_arrray - this is the function that perfoms the task
+ * Return:  pointer to the array, or NULL if it fails
+ * @size:  the size of the array
+ * @c: members of the array
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *ptr;
+	unsigned int i;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	ptr = (char *)malloc(size * sizeof(c));
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < size; i++)
+	{
+		ptr[i] = c;
+	}
+	return (ptr);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,11 @@
+this is the read me for thids directory
+malloc is used to allocate dynamic memory in c langusgeit returns a
+ a void poointr which you can cast it to any type
+
+*ptr = int maloc(size)
+realloc is used to reallocate  it takes 2 args   the ptr and the size
+
+calloc also is used to allocate the size
+
+
+freee frees the memory  takes one arg of the ptr
