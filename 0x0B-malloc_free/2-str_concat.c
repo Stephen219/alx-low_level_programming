f38995d57c5c529749c9b503b6332a39a91548c4
Stephen Kariuki <stephenkariuki838@gmail.com>
2023-10-02 12:49:50 -0700

    task on concatination  trying but failing

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -31,11 +31,7 @@ char *str_concat(char *s1, char *s2)
 	if (s2 != NULL)
 	{
 		strcat(result, s2);
-	}
-	if (s1 == NULL && s2 == NULL)
-        {
-                return (NULL);
-        }
+	}}
 
 
 	return (result);
