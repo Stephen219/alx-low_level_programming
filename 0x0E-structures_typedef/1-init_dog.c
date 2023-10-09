13670463d6831fb5626f56d2b2507a91ad5c4b8f
Stephen Kariuki <stephenkariuki838@gmail.com>
2023-10-09 13:20:34 -0700

    task 1 and initial commit corrected but not working

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,18 +1,19 @@
+#include <stdlib.h>
 #include "dog.h"
-#include <stdio.h>
+
 /**
- * init_dog  - this is the function to make the struct
- * @d: the structure
- * @name: name
- * @age: the int age
- * @owner: who owns
+ * init_dog - initializes a variable of type struct dog
+ * @d: pointer to struct dog to initialize
+ * @name: name to initialize
+ * @age: age to initialize
+ * @owner: owner to initialize
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (d != NULL)
-	{
-		d->name = name;
-		d->age = age;
-		d->owner = owner;
-	}
+	if (d == NULL)
+		d = malloc(sizeof(struct dog));
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
 }
+
