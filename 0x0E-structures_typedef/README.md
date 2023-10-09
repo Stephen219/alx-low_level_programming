9af607085d53f1881912e267e323426af699cdb1
Stephen Kariuki <stephenkariuki838@gmail.com>
2023-10-09 12:36:32 -0700

    task 0 and initial commit

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,3 @@
+ this is the readme for this directory on c structures and typedef   
+struct  can equate a constructor in java 
+
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ *struct Dog - the structure dog with names age and owner
+ * @name: the name
+ * @age: age
+ * @owner: who owns
+ */
+struct Dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
