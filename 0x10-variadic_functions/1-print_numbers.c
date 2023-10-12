e546af9951ad7c1d174da20982330be30d6f41dc
Stephen Kariuki <stephenkariuki838@gmail.com>
2023-10-12 09:31:19 -0700

    task 2 on printing args

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,26 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+/**
+ * print_numbers - the function that prints numbers
+ * @separator: the thing to be printed in between texts
+ * @n: the number of items args
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list list;
+	int v;
+	unsigned int i;
+
+	va_start(list, n);
+
+	for (i = 0; i < n; i++)
+	{
+		v = va_arg(list, int);
+		printf("%d", v);
+		printf("%s", separator);
+
+	}
+	printf("\n");
+}
