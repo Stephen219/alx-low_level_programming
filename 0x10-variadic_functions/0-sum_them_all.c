b43b61231fb041041903a2efcba8b48fd011494d
Stephen Kariuki <stephenkariuki838@gmail.com>
2023-10-12 09:06:55 -0700

    initial commit and task 0

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdarg.h>
+/**
+ * sum_them_all the function that ssums all the args
+ * @n: the first argument
+ * @...: all the other params
+ * Return: returns the sum as an int for a success event.
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	int sum = 0;
+	unsigned int i;
+
+	va_list arg_list;
+
+	va_start(arg_list, n);
+
+	for (i = 0; i< n; i++)
+	{
+		sum = sum + va_arg(arg_list, int);
+	}
+	return (sum);
+}
---
+++ 0x10-variadic_functions/README.md
@@ -0,0 +1,3 @@
+this is the resd me for the variadic functions
+this are functions that take unspecified no of args enabled by the the stdarg.h
+
