38545586aef8a413436df7a83d4e27636e4e3fdd
Stephen Kariuki <stephenkariuki838@gmail.com>
2023-10-12 09:46:43 -0700

    fixed the output error   no comma at the end

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -17,13 +17,16 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	for (i = 0; i < n; i++)
 	{
+		if (separator == NULL)
+		{
+			printf("%d", v);
+		}
 		if (i < n)
 		{
 			v = va_arg(list, int);
 			printf("%d", v);
 			printf("%s", separator);
 		}
-		 printf("%d", v);
 	}
 	printf("\n");
 }
