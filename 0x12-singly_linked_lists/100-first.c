25d6149ac921b87912f47ecde4a36c7f49837e50
Stephen Kariuki <stephenkariuki838@gmail.com>
2023-10-19 14:37:57 -0700

    task 5 and betty

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+
+#include <stdio.h>
+
+void first(void) __attribute__ ((constructor));
+
+/**
+ * first - prints a sentence before the main iis executed
+ * function is executed
+ * Return: void
+ */
+void first(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
