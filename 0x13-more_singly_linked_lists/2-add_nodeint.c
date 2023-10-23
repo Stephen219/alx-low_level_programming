9fd8b2feca26c9d168ec6cdcbc52c059a0b46d93
Stephen Kariuki <stephenkariuki838@gmail.com>
2023-10-23 11:11:22 -0700

    correction

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,39 +1,36 @@
-
 #include "lists.h"
 
 /**
- * add_nodeint_end - Adds a new node at the end of a listint_t list.
- * @head: Pointer to a pointer to the head of the list.
- * @n: The integer value to be added to the new node.
- *
- * Return: The address of the new element, or NULL if it fails.
+ * add_node - function that adds a node to the list
+ * Return: the liked list
+ * @head:  pointer to a pointer
+ * @str: to be added
  */
-listint_t *add_nodeint(listint_t **head, const int n)
+
+
+list_t *add_node(list_t **head, const char *str)
 {
-	listint_t *new_node, *temp;
+	list_t *new;
 
-
-	new_node = malloc(sizeof(listint_t));
-
-	if (new_node == NULL)
+	if (!str)
 	{
 		return (NULL);
 	}
-	new_node->n = n;
-	new_node->next = NULL;
-	if (*head == NULL)
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
 	{
-		*head = new_node;
-	}
-	else
-	{
-		temp = *head;
-		while (temp->next != NULL)
-		{
-			temp = temp->next;
-			temp->next = new_node;
-		}
-	}
-	return (new_node);
-}
+		return (NULL);
 
+	}
+	new->str = strdup(str);
+
+	if (new->str == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+	new->len = strlen(new->str);
+	new->next = *head;
+	*head = new;
+	return (new);
+}
