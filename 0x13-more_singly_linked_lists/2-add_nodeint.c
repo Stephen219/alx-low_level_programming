473f7de769db843657bc7f59857cb44b0018b9f5
Stephen Kariuki <stephenkariuki838@gmail.com>
2023-10-23 11:04:30 -0700

    bettyy

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -8,12 +8,12 @@
  *
  * Return: The address of the new element, or NULL if it fails.
  */
-listint_t *add_nodeint_end(listint_t **head, const int n)
+listint_t* add_nodeint(listint_t **head, const int n)
 {
 	listint_t *new_node, *temp;
 
 
-	new_node = malloc(sizeof(listint_t);
+	new_node = malloc(sizeof(listint_t));
 
 	if (new_node == NULL)
 	{
@@ -28,7 +28,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	else
 	{
 		temp = *head;
-		while (temp->next != NULL
+		while (temp->next != NULL)
 		{
 			temp = temp->next;
 			temp->next = new_node;
