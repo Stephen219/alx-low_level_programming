6ba8bc385d959943796fc10b415c807d8f533706
Stephen Kariuki <stephenkariuki838@gmail.com>
2023-10-23 10:27:14 -0700

    initial commit

---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,39 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * add_nodeint_end - Adds a new node at the end of a listint_t list.
+ * @head: Pointer to a pointer to the head of the list.
+ * @n: The integer value to be added to the new node.
+ *
+ * Return: The address of the new element, or NULL if it fails.
+ */
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *new, *t;
+
+
+	new = malloc(sizeof(listint_t);
+
+	if (new == NULL)
+	{
+		return (NULL);
+	}
+	new->n = n;
+	new->next = NULL;
+	if (*head == NULL)
+	{
+		*head = new;
+	}
+	else
+	{
+		t = *head;
+		while (t->next != NULL)
+		{
+			t = t->next;
+			t->next = new;
+		}
+	}
+	return (new);
+}
+
---
+++ 0x13-more_singly_linked_lists/README.md
@@ -0,0 +1,13 @@
+this is the readme for this directory       on more linked list  
+`0-print_listint.c` : prints all the elements
+`1-listint_len.c`: number of elements 
+`2-add_nodeint.c`:  adds a new node at the beginning of a
+`3-add_nodeint_end.c`: adds a new node at the end of a 
+`4-free_listint.c`:hat frees a listint_t list.
+`5-free_listint2.c`:ees a listint_t list.
+
+`6-pop_listint.c`:deletes the head node of a listint_t linked list, and returns the head node’s data (n).
+
+` 7-get_nodeint.c`: that returns the nth node of a listint_t linked list.
+`8-sum_listint.c`: inserts a new node at a given position.
+
