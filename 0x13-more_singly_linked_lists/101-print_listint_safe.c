6a4f5010829b5598f9948f3633ca4cde6f525d78
Stephen Kariuki <stephenkariuki838@gmail.com>
2023-10-24 15:58:01 -0700

    task 10

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -12,30 +12,21 @@ size_t print_listint_safe(const listint_t *head)
 {
 	size_t count = 0;
 	const listint_t *current = head;
-	const listint_t *slow = head;
-	const listint_t *fast = head;
-
-	if (head == NULL)
-		exit(98);
-	while (fast != NULL && fast->next != NULL)
-	{
-		slow = slow->next;
-		fast = fast->next->next;
-		if (slow == fast)
-		{
-			printf("-> [%p] %d\n", (void *)slow, slow->n);
-			return (count);
-		}
+	const listint_t *check = head;
 
 
-	}
 
 	while (current != NULL)
 	{
 		printf("[%p] %d\n", (void *)current, current->n);
-		current = current->next;
 		count++;
+		current = current->next;
+		
+	
 	}
+	if (current == check)
+		exit(98);
+
 	return (count);
 }
 
