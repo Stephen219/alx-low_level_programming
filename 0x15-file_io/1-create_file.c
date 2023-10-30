8f50ee14394279d054b131da06c698bf7be377e3
Stephen Kariuki <stephenkariuki838@gmail.com>
2023-10-30 11:26:59 -0700

    task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,47 +1,38 @@
 #include "main.h"
 
 /**
- * create_file - Creates a file with specified permissions and writes content.
- * @filename: The name of the file to create.
- * @text_content: The text content to write to the file (can be NULL).
+ * create_file - creates a file in te system
+ * @filename: to be createdd filename.
+ * @text_content: text to be written in in the dfile
  *
- * Return: 1 on success, -1 on failure
- *
+ * Return: 1 if it success. -1 if it fails.
  */
-
-
 int create_file(const char *filename, char *text_content)
 {
-	int fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	int fd;
+	int no_letters;
+	int rwr;
 
-
-
-	if (filename == NULL)
-	{
+	if (!filename)
 		return (-1);
-	}
+
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
 	if (fd == -1)
+		return (-1);
+
+	if (!text_content)
+		text_content = "";
+
+	for (no_letters = 0; text_content[no_letters]; no_letters++);
+	rwr = write(fd, text_content, no_letters);
+
+	if (rwr == -1)
 	{
 		return (-1);
 	}
 
-
-	if (text_content != NULL)
-	{
-		ssize_t bytes_written = write(fd, text_content, strlen(text_content));
-
-		if (bytes_written == -1)
-		{
-			close(fd);
-			return (-1);
-
-		}
-	}
-
-
 	close(fd);
+	return (1);
 
-	return (-1);
-
-}
+}	
